120a6631233e777cbc56cdace362ed8d26eff7e8
NicolayB <ne.barrera@uniandes.edu.co>
2024-09-08 17:16:35 -0500

    Modificacion DataFrame y conversion de la fecha

--- Proyecto1_Nicolay_Daniel.py
+++ Proyecto1_Nicolay_Daniel.py
@@ -9,3 +9,15 @@ print(path)
 
 data = pd.read_csv("SeoulBikeData_utf8.csv")
 print(data.head())
+
+# Pasar la variable de interés al final del DataFrame
+v_interes = data.pop("Rented Bike Count")
+data["Rented Bike Count"] = v_interes
+print(data)
+
+# Convertir la columna fecha en formato fecha
+data["Date"] = pd.to_datetime(data["Date"], format="%d/%m/%Y")
+print(data)
+
+# Valores faltantes
+print(data.isnull().sum())
