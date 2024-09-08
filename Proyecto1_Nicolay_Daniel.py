72cbc29365aa7c555fadc20e9e12a80ad7642aec
NicolayB <ne.barrera@uniandes.edu.co>
2024-09-08 17:39:00 -0500

    Conversion variables categoricas

--- Proyecto1_Nicolay_Daniel.py
+++ Proyecto1_Nicolay_Daniel.py
@@ -28,3 +28,37 @@ print(data.describe())
 # Tipos de datos
 types = data.dtypes.value_counts()
 print(f"Tipos de variables en los datos: \n{types}")
+
+print(data.columns)
+
+# Variables tipo objeto
+print(data["Seasons"].unique())
+print(data["Seasons"].value_counts())
+print(data["Holiday"].unique())
+print(data["Holiday"].value_counts())
+print(data["Functioning Day"].unique())
+print(data["Functioning Day"].value_counts())
+# Convertir en enteros las variables tipo objeto
+data["Holiday"] = data["Holiday"].map({"Holiday": 1, "No Holiday":0})
+data["Functioning Day"] = data["Functioning Day"].map({"Yes": 1, "No":0})
+print(data)
+# Variable Seasons
+data["Winter"] = data["Seasons"].map({"Winter":1, "Spring":0, "Summer":0, "Autumn":0})
+data["Spring"] = data["Seasons"].map({"Winter":0, "Spring":1, "Summer":0, "Autumn":0})
+data["Summer"] = data["Seasons"].map({"Winter":0, "Spring":0, "Summer":1, "Autumn":0})
+data["Autumn"] = data["Seasons"].map({"Winter":0, "Spring":0, "Summer":0, "Autumn":1})
+data.drop("Seasons", axis=1, inplace=True)
+
+v_interes = data.pop("Rented Bike Count")
+data["Rented Bike Count"] = v_interes
+print(data)
+
+# Verificar que se hizo la transformación
+print(data["Winter"].value_counts())
+print(data["Spring"].value_counts())
+print(data["Summer"].value_counts())
+print(data["Autumn"].value_counts())
+print(data["Holiday"].unique())
+print(data["Holiday"].value_counts())
+print(data["Functioning Day"].unique())
+print(data["Functioning Day"].value_counts())
